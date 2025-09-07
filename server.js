f0cd514503492ce04c903bd9d658e3ab2a028c68
vmquanglc <vmquanglc@gmail.com>
2025-09-07 13:16:04 +0700

    Thêm cors

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const http = require("http");
+const cors = require("cors");
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+
+app.use(cors());
+
 const io = new Server(server, {
   cors: { origin: "*" },
-  methods: ["GET", "POST"],
+  methods: ["GET", "POST"]
 });
 
 const SIZE = 18;
